0bc11cb83af49d951d93d33b1639b4d18c64c62a
Grebnik89 <116430298+art-just-code@users.noreply.github.com>
2024-11-28 22:21:25 +0300

    add all files and README.md

--- README.md
+++ README.md
@@ -1,5 +1,9 @@
-Look at the branches :)
+docker - ci/cd pipline with release and deploy use by docker
 
-notification - into TG
+Java backend + Angular frontend, postgreSQL + MongoDB databases
 
-nexus-upload - upload stage into Nexus
+Сборки бэкенда и фронтенда происходят в джобах build, сборщик gcr.io/kaniko-project/executor:v1.9.0-debug
+Далее тесты, для бэка добавлен -Dmaven.test.skip=true чтобы Сонар не лез в бд
+Далее релиз в хранилище Gitlab Container Registry
+Далее деплой из этого хранилища на вм
+
---
+++ frontend/deploy.sh
@@ -0,0 +1,13 @@
+#! /bin/bash
+#Если свалится одна из команд, рухнет и весь скрипт
+set -xe
+sudo docker login -u ${CI_REGISTRY_USER} -p ${CI_REGISTRY_PASSWORD} ${CI_REGISTRY}
+sudo docker network create -d bridge sausage_network || true # пробуем создать сеть, если не получилось - прокидывает true, чтобы скрипт не упал
+sudo docker rm -f sausage-frontend || true
+sudo docker run -d --name sausage-frontend \
+    -p 80:80 \
+    -v "/tmp/${CI_PROJECT_DIR}/frontend/default.conf:/etc/nginx/conf.d/default.conf" \
+    --restart=on-failure:10 \
+    --restart=always \
+    --network=sausage_network \
+    "${CI_REGISTRY_IMAGE}"/sausage-frontend:${VERSION}
